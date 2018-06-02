3efbaf9c155d3baffef3059b00fab49c1230e6fd
xavic01 <32305674+xavic01@users.noreply.github.com>
2018-06-02 16:15:50 +0200

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,11 +1,11 @@
-//
+// Creamos la variable
 var archivo=1;
 
 
 
 $(document).ready(function() {
 
-
+//La función del scroll para que se carguen las noticias sólo bajando el cursor
 	$(window).scroll(function() {
 		if ($(document).height() - $(window).height()  == $(window).scrollTop()) {					
 			cargarNoticias();
@@ -19,7 +19,7 @@ $(document).ready(function() {
 
 });
 
-//
+//Creamos la función que sirve para que se carguen más noticias con el botón de cargar más que se encuentran dentro de dos archivos .json
 function cargarNoticias(){
     //
     if(archivo<3){
@@ -30,7 +30,7 @@ function cargarNoticias(){
 	}
 }
 
-//
+//Insertamos la noticia dentro del archivo HTML
 function insertarNoticia(json){
    $.each(json, function(i,noticia){
 $("#noticias").append("<hr><div id='imgDiv'><img class='img-thumbnail' id='imgNoticia' src="+noticia.img+" alt="+noticia.alt+"></div><div id='news' class='container' id='cuerpo'><a href='#'><h3>"+noticia.title+"</h3></a><p>"+noticia.desc+"</p><footer id='date' class='small'>"+noticia.date+"</footer></div>");
